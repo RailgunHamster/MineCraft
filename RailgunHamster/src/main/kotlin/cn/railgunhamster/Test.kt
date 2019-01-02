15c070954dd79eef603309aa2724ccac3d9acd1b
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2019-01-02 09:48:47 +0800

    Update Test.kt

--- RailgunHamster/src/main/kotlin/cn/railgunhamster/Test.kt
+++ RailgunHamster/src/main/kotlin/cn/railgunhamster/Test.kt
@@ -1,4 +1,4 @@
-package cn.railgunhamster.Test
+package cn.railgunhamster
 
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
@@ -14,15 +14,15 @@ import org.apache.logging.log4j.Logger;
 	version = Test.VERSION,
 	acceptedMinecraftVersions = Test.MC_VERSION,
 	dependencies = """required-after:forge@[14.23.4.2749,);""",
-	modLanguageAdapter = "cn.shadowfacts.forgelin.KotlinAdapter"
+	modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter"
 )
 object Test {
-	public val NAME				= "Test"
-	public val MOD_ID 		= NAME.toLowerCase()
-	public val VERSION		= "1.0"
-	public val MC_VERSION = "[1.12.2, 1.13)"
+	const val NAME			 = "Test"
+	const val MOD_ID 		 = "test"
+	const val VERSION		 = "1.0"
+	const val MC_VERSION = "[1.12.2, 1.13)"
 
-	var logger: Logger?
+	var logger: Logger? = null
 
 	@Mod.EventHandler
 	fun preInit(event: FMLPreInitializationEvent) {
@@ -31,6 +31,6 @@ object Test {
 
 	@Mod.EventHandler
 	fun init(event: FMLInitializationEvent) {
-		this.logger.info("Hello from Kotlin!")
+		this.logger?.info("Hello from Kotlin!")
 	}
 }
